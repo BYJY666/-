6c56b1b2f792a68b351608134be02f83847e1602
BYJY666 <109867757+BYJY666@users.noreply.github.com>
2022-09-04 01:50:04 +0800

    背景动态变化的登录界面

--- 背景动态变化的登录界面/index.js
+++ 背景动态变化的登录界面/index.js
@@ -1,6 +1,5 @@
 const container = document.querySelector('.container');
 const btn_login = document.querySelector('.btn-login');
-
 btn_login.addEventListener('click', function () {
   container.classList.add('success');
 })
